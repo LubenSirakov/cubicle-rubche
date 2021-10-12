68f1eb866efa85778df0cba4eebd160fa2969b6e
Luben Sirakov <lubo95sirakov@gmail.com>
2021-10-12 20:57:03 +0300

    add login post action

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,12 @@ const router = require('express').Router();
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
+});
+
+router.post('/login', (req, res) => {
+    console.log(req.body);
+
+    res.redirect('/login');
 })
 
 module.exports = router;
