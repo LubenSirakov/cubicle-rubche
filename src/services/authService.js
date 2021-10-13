0bdf34cf985434903dc4593dc23fe4e72287df6b
Luben Sirakov <lubo95sirakov@gmail.com>
2021-10-13 11:06:48 +0300

    create jwt token on login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,8 @@
-const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcrypt');
 const User = require('../models/User.js');
+const { jwtSign } = require('../utils/jwtUtils.js');
+const { SECRET } = require('../constants.js');
 
 exports.register = function (username, password, repeatPassword) {
     // return bcrypt.hash(password, 10)
@@ -12,7 +15,7 @@ exports.login = function (username, password) {
     return User.findByUsername(username)
         .then(user => Promise.all([user.validatePassword(password), user]))
         .then(([isValid, user]) => {
-            
+
             if (isValid) {
                 return user;
             } else {
@@ -21,3 +24,14 @@ exports.login = function (username, password) {
         })
         .catch(() => null);
 };
+
+exports.createToken = function (user) {
+    let payload = {
+        _id: user._id,
+        username: user.username,
+
+    }
+
+    return jwtSign(payload, SECRET);
+};
+
