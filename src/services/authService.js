f5bea3c13f668a458e4aa119321008a9be893c7b
Luben Sirakov <lubo95sirakov@gmail.com>
2021-10-13 10:12:24 +0300

    verify password on login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,9 +1,22 @@
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const User = require('../models/User.js');
 
 exports.register = function (username, password, repeatPassword) {
     // return bcrypt.hash(password, 10)
     //     .then(hash => User.create({ username, password: hash }))
 
-    return User.create({username, password})
+    return User.create({ username, password })
+};
+
+exports.login = function (username, password) {
+    return User.findByUsername(username)
+        .then(user => Promise.all([bcrypt.compare(password, user.password), user]))
+        .then(([isValid, user]) => {
+            console.log(values);
+            if (isValid) {
+                return user;
+            } else {
+                throw { message: 'Cannot find username or password!' }
+            }
+        })
 };
